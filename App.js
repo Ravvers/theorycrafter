dd3d5f30619afa66f3b718fa1e225af1dc1a9101
Raveen <rmaldeni@gmail.com>
2021-01-23 15:19:37 +1300

    Uninstalled react-native-video. Installed expo-av and added IntroVideo

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Video } from 'expo-av';
+import IntroVideo from './assets/videos/intro.mp4';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Video
+  source={IntroVideo}
+  rate={1.0}
+  volume={1.0}
+  isMuted={false}
+  resizeMode="cover"
+  shouldPlay
+  isLooping
+  style={{ width: 500, height: 500 }}
+/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
